8b5fdf0b52d78d3748c60f778632472541ae08a7
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-09-17 15:19:13 +0200

    File just to store all imageGridData to make use of Data-driven React. Makes it easier to add more images for the grid later on, or change one, or more images without having to change any of my components

---
+++ imageGridData.js
@@ -0,0 +1,28 @@
+import { nanoid } from "nanoid";
+
+export default [
+    {
+        id: nanoid(),
+        image: "",
+        supportingText: "",
+        linkTo: ""
+    },
+    {
+        id: nanoid(),
+        image: "",
+        supportingText: "",
+        linkTo: ""
+    },
+    {
+        id: nanoid(),
+        image: "",
+        supportingText: "",
+        linkTo: ""
+    },
+    {
+        id: nanoid(),
+        image: "",
+        supportingText: "",
+        linkTo: ""
+    }
+];
