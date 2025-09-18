191dfdc5115e8ca179d69e9dc65dd368d7598081
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-09-18 19:27:05 +0200

    Removed some classnames because I changed the order of the homepage which ended up to me needing to change the styling as well

--- src/components/Home/AboutUs.jsx
+++ src/components/Home/AboutUs.jsx
@@ -7,12 +7,12 @@ export default function AboutUs() {
             <div className="about-us max-width-1400 side-margin-auto">
                 <img src={aboutUsImg} alt="Eigenaar Richard Rosink en zijn vrouw, Bianca Rosink, poserend voor het restaurant." />
                 <div className="txt-container">
-                    <h2 className="font-size-24 font-weight-400 color-light">Over ons</h2>
+                    <h2 className="font-size-24 font-weight-400">Over ons</h2>
                     <div className="desc">
-                        <p className="color-light">Ons restaurant bestaat al meer dan _aantal_ jaar en is een vaste waarde in de stad. In _jaar_ werd het overgenomen door onze huidige eigenaar en chef, die met passie en vernieuwende ideeën de traditie voortzet. Hier komen gastvrijheid, smaak en beleving samen.</p>
-                        <p className="color-light">Ons restaurant bestaat al meer dan _aantal_ jaar en is een vaste waarde in de stad. In _jaar_ werd het overgenomen door onze huidige eigenaar en chef, die met passie en vernieuwende ideeën de traditie voortzet. Hier komen gastvrijheid, smaak en beleving samen.</p>
+                        <p>Ons restaurant bestaat al meer dan _aantal_ jaar en is een vaste waarde in de stad. In _jaar_ werd het overgenomen door onze huidige eigenaar en chef, die met passie en vernieuwende ideeën de traditie voortzet. Hier komen gastvrijheid, smaak en beleving samen.</p>
+                        <p>Ons restaurant bestaat al meer dan _aantal_ jaar en is een vaste waarde in de stad. In _jaar_ werd het overgenomen door onze huidige eigenaar en chef, die met passie en vernieuwende ideeën de traditie voortzet. Hier komen gastvrijheid, smaak en beleving samen.</p>
                     </div>
-                    <Link to="about" className="btn sec-btn">Lees meer over ons</Link>
+                    <Link to="about" className="btn primary-btn">Lees meer over ons</Link>
                 </div>
             </div>
         </div>
