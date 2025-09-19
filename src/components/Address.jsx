bbc60375400a3c08cb78803673954e416cad7e68
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-09-19 16:30:32 +0200

    Added all the address info

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -1,7 +1,12 @@
 export default function Address() {
     return (
-        <div className="address-container">
-            <h1 className="color-light">Address</h1>
+        <div className="container">
+            <h2 className="color-light font-size-32">Adres</h2>
+            <div className="address">
+                <p className="color-light">IJsselkade 59</p>
+                <p className="color-light">8261AG, Kampen</p>
+                <p className="color-light">Overijssel</p>
+            </div>
         </div>
     );
 }
