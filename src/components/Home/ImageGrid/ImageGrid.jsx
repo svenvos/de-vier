026563b3782cd73517b6f090603736712317bfe5
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-09-19 12:52:03 +0200

    Imported the Link component, and created an array which holds all imageGridElements, by looping over the imageGridData and rendering an ImageCard for every item in the array. Also rendered the imageGridElements in a div

--- src/components/Home/ImageGrid/ImageGrid.jsx
+++ src/components/Home/ImageGrid/ImageGrid.jsx
@@ -1,8 +1,23 @@
+import { Link } from "react-router";
 import ImageCard from "./ImageCard.jsx";
 import imageGridData from "../../../../imageGridData.js";
 
 export default function ImageGrid() {
+    const imageGridElements = imageGridData.map(item => {
+        return (
+            <ImageCard key={item.id}>
+                <div className="image-wrapper">
+                    <img src={item.imageSrc} alt={item.imageAlt} />
+                </div>
+                <p className="font-size-18">{item.supportingText}</p>
+                <Link to={item.linkTo}>{item.btnTxt}</Link>
+            </ImageCard>
+        );
+    });
+
     return (
-        <h1>Image Grid</h1>
+        <div className="image-grid-container">
+            {imageGridElements}
+        </div>
     );
 }
