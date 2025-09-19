61e1bbff81d521dfdd797b86b20e98aad5b33353
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-09-19 12:42:27 +0200

    Imported all needed images for the image grid. Also added all of the needed text, and links for the first 2 images of the grid

--- imageGridData.js
+++ imageGridData.js
@@ -1,28 +1,41 @@
 import { nanoid } from "nanoid";
+import morningBuffetImg from "./src/assets/imageGridImages/morning-buffet.jpg";
+import barImg from "./src/assets/imageGridImages/bar.jpg";
+import dinerbonImg from "./src/assets/imageGridImages/dinerbon.jpg";
+import fishBuffetImg from "./src/assets/imageGridImages/fish-buffet.jpg";
+import wrapsBuffetImg from "./src/assets/imageGridImages/wraps-buffet.jpg";
 
 export default [
     {
         id: nanoid(),
-        image: "",
+        imageSrc: morningBuffetImg,
+        imageAlt: "Voorbeeld van een heerlijk uitgebreid ochtendbuffet met verschillende soorten vleeswaren en broodjes.",
+        supportingText: "Voorbeeld van een ontbijtbuffet",
+        btnTxt: "Bekijk onze buffetten",
+        linkTo: "buffet"
+    },
+    {
+        id: nanoid(),
+        imageSrc: barImg,
+        imageAlt: "Sfeervolle foto van de bar. Verschillende taps voor bieren te zien, met leuke barkrukken aan de bar.",
+        supportingText: "Onze prachtige bar",
+        btnTxt: "Bekijk ons menu",
+        linkTo: "menu"
+    },
+    {
+        id: nanoid(),
+        imageSrc: dinerbonImg,
+        imageAlt: "",
         supportingText: "",
+        btnTxt: "",
         linkTo: ""
     },
     {
         id: nanoid(),
-        image: "",
+        imageSrc: "",
+        imageAlt: "",
         supportingText: "",
-        linkTo: ""
-    },
-    {
-        id: nanoid(),
-        image: "",
-        supportingText: "",
-        linkTo: ""
-    },
-    {
-        id: nanoid(),
-        image: "",
-        supportingText: "",
+        btnTxt: "",
         linkTo: ""
     }
 ];
