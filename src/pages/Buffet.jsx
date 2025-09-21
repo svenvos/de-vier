8731a66796c009350f09930d31758d673755a149
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-09-21 13:04:53 +0200

    Imported the BuffetMenu component and the CSS file, also added all needed elements for this page

--- src/pages/Buffet.jsx
+++ src/pages/Buffet.jsx
@@ -1,5 +1,24 @@
+import BuffetMenu from "../components/BuffetMenu";
+import "../styles/Buffet.css";
+
 export default function Buffet() {
     return (
-        <h1>Buffet</h1>
+        <main>
+            <div className="buffet-container">
+                <h1>Buffet</h1>
+                <p>Mocht u één of meerdere buffetten willen reserveren, gelieve contact met ons opnemen via telefoon of email.</p>
+                <div className="contact-details">
+                    <p className="font-size-20 font-weight-600">
+                        Tel: 
+                        <a href="tel:038-33 334 63" className="font-size-16 font-weight-400">038-33 334 63</a>
+                    </p>
+                    <p className="font-size-20 font-weight-600">
+                        Mail: 
+                        <a href="mailto:info@devier-kampen.nl" className="font-size-16 font-weight-400">info@devier-kampen.nl</a>
+                    </p>
+                </div>
+                <BuffetMenu />
+            </div>
+        </main>
     );
 }
