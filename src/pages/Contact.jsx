01f71af12d98166fd4eacb2ee33e7f1c8db14aba
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-09-21 15:59:55 +0200

    Contact page done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,13 @@
+import ContactDetails from "../components/ContactDetails";
+
 export default function Contact() {
     return (
-        <h1>Contact</h1>
+        <main>
+            <div className="container">
+                <h1>Contact</h1>
+                <p>Mocht u vragen en/of specifieke wensen hebben, kunt u contact met ons opnemen via onderstaande gegevens.</p>
+                <ContactDetails />
+            </div>
+        </main>
     );
 }
