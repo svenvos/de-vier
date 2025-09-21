3c99e1dbbe0bd1894b977a3ba2ade05a4a3cab35
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-09-21 16:49:03 +0200

    Imported all Menu components and created the Routes, and nested Routes in order for the user to move around the different menus

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import ScrollToTop from "./components/ScrollToTop.jsx";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
-import Menu from "./pages/Menu.jsx";
 import Buffet from "./pages/Buffet.jsx";
 import Dinerbon from "./pages/Dinerbon.jsx";
 import AboutUs from "./pages/AboutUs.jsx";
 import Calendar from "./pages/Calendar.jsx";
 import Contact from "./pages/Contact.jsx";
+import MenuLayout from "./components/MenuLayout.jsx";
+import DinnerMenu from "./components/Menu/DinnerMenu.jsx";
+import LunchMenu from "./components/Menu/LunchMenu.jsx";
+import DessertsMenu from "./components/Menu/DessertsMenu.jsx";
 
 export default function App() {
   return (
@@ -17,7 +20,11 @@ export default function App() {
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
-                <Route path="menu" element={<Menu />} />
+                <Route path="menu" element={<MenuLayout />}>
+                    <Route index element={<DinnerMenu />} />
+                    <Route path="lunch" element={<LunchMenu />} />
+                    <Route path="desserts" element={<DessertsMenu />} />
+                </Route>
                 <Route path="buffet" element={<Buffet />} />
                 <Route path="dinerbon" element={<Dinerbon />} />
                 <Route path="about" element={<AboutUs />} />
