f22afa08b2b91eeeb7c1af6e161ab037ea7f9c45
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-09-21 14:06:39 +0200

    Added a socials container to the footer, and styled it

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { FaInstagram, FaFacebookF } from "react-icons/fa6";
 import OpeningHours from "./OpeningHours.jsx";
 import Address from "./Address.jsx";
 import ContactDetails from "./ContactDetails.jsx";
@@ -14,6 +15,17 @@ export default function Footer() {
             <div className="container">
                 <h2 className="color-light font-size-32">Contact</h2>
                 <ContactDetails className="color-light" />
+            </div>
+            <div className="container">
+                <h2 className="color-light">Socials</h2>
+                <div className="icons">
+                    <a href="https://www.instagram.com/de_vierkampen/" target="_blank" className="color-light">
+                        <FaInstagram className="font-size-24" />
+                    </a>
+                    <a href="https://www.facebook.com/devierkampen" target="_blank" className="color-light">
+                        <FaFacebookF className="font-size-24" />
+                    </a>
+                </div>
             </div>
         </footer>
     );
