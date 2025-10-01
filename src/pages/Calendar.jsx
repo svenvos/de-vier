97a92f950b3b82e291def675737a7131928e0c0f
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-10-01 11:13:40 +0200

    Added the skeleton for the page

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,5 +1,11 @@
+import "../styles/Calendar.css";
+
 export default function Calendar() {
     return (
-        <h1 className="font-size-32">Calendar</h1>
+        <main>
+            <div className="container">
+                <h1 className="font-size-32">Calendar</h1>
+            </div>
+        </main>
     );
 }
--- src/pages/Dinerbon.jsx
+++ src/pages/Dinerbon.jsx
@@ -1,5 +1,11 @@
+import "../styles/Dinerbon.css";
+
 export default function Dinerbon() {
     return (
-        <h1 className="font-size-32">Dinerbon</h1>
+        <main>
+            <div className="container">
+                <h1 className="font-size-32">Dinerbon</h1>
+            </div>
+        </main>
     );
 }
