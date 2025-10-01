8844fa2336962286d9ce89361a0baba1e7624625
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-10-01 11:12:01 +0200

    Added a skeleton for the about us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,14 @@
+import "../styles/AboutUs.css";
+
 export default function() {
     return (
-        <h1 className="font-size-32">Over ons</h1>
+        <main>
+            <div className="container">
+                <h1 className="font-size-32">Over ons</h1>
+                <p>Ons restaurant bestaat al meer dan _aantal_ jaar en is een vaste waarde in de stad. In _jaar_ werd het overgenomen door onze huidige eigenaar en chef, die met passie en vernieuwende ideeën de traditie voortzet. Hier komen gastvrijheid, smaak en beleving samen.</p>
+                <p>Ons restaurant bestaat al meer dan _aantal_ jaar en is een vaste waarde in de stad. In _jaar_ werd het overgenomen door onze huidige eigenaar en chef, die met passie en vernieuwende ideeën de traditie voortzet. Hier komen gastvrijheid, smaak en beleving samen.</p>
+                <p>Ons restaurant bestaat al meer dan _aantal_ jaar en is een vaste waarde in de stad. In _jaar_ werd het overgenomen door onze huidige eigenaar en chef, die met passie en vernieuwende ideeën de traditie voortzet. Hier komen gastvrijheid, smaak en beleving samen.</p>
+            </div>
+        </main>
     );
 }
