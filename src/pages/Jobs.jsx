35eb7a4398ce774a3de0891f5f5e16333a21cd4c
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-10-03 19:36:39 +0200

    New jobs page

---
+++ src/pages/Jobs.jsx
@@ -0,0 +1,9 @@
+export default function Jobs() {
+    return (
+        <main>
+            <div className="container">
+                <h1 className="font-size-32">Vacatures</h1>
+            </div>
+        </main>
+    );
+}
