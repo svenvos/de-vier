1387b139a659ca543fdc3b6e1b5639cbf2572d8d
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-10-09 14:45:18 +0200

    Removed the emojis from the text because it made it look cheap

--- src/utils/imageGridData.js
+++ src/utils/imageGridData.js
@@ -3,7 +3,6 @@ import isSeasonalPeriod from "./dateUtils.js";
 import morningBuffetImg from "../assets/imageGridImages/morning-buffet.jpg";
 import barImg from "../assets/imageGridImages/bar.jpg";
 import dinerbonImg from "../assets/imageGridImages/dinerbon.jpg";
-import fishBuffetImg from "../assets/imageGridImages/fish-buffet.jpg";
 import christmasImg from "../assets/imageGridImages/christmas.jpg";
 
 let imageGridData = [
@@ -11,7 +10,7 @@ let imageGridData = [
         id: nanoid(),
         imageSrc: dinerbonImg,
         imageAlt: "Onze prachtige dinerbon voor een vaas met bloemen, naast een fles wijn.",
-        supportingText: "Geef de smaak van genieten cadeau 🎁",
+        supportingText: "Geef de smaak van genieten cadeau",
         btnTxt: "Bekijk onze dinerbon",
         linkTo: "dinerbon"
     },
@@ -19,7 +18,7 @@ let imageGridData = [
         id: nanoid(),
         imageSrc: morningBuffetImg,
         imageAlt: "Voorbeeld van een heerlijk uitgebreid ochtendbuffet met verschillende soorten vleeswaren en broodjes.",
-        supportingText: "Samen genieten met onze groepsarrangementen 🍷",
+        supportingText: "Samen genieten met onze groepsarrangementen",
         btnTxt: "Bekijk onze arrangementen",
         linkTo: "buffet"
     },
@@ -27,7 +26,7 @@ let imageGridData = [
         id: nanoid(),
         imageSrc: barImg,
         imageAlt: "Sfeervolle foto van de bar. Verschillende taps voor bieren te zien, met leuke barkrukken aan de bar.",
-        supportingText: "Een kijkje in ons restaurant ✨",
+        supportingText: "Een kijkje in ons restaurant",
         btnTxt: "Bekijk ons menu",
         linkTo: "menu"
     }
@@ -38,7 +37,7 @@ if (isSeasonalPeriod()) {
         id: nanoid(),
         imageSrc: christmasImg,
         imageAlt: "Nette, gedekte tafel in kerstsferen. Met kerstverlichting aan de wand en een cadeau op tafel.",
-        supportingText: "Vier de feestdagen bij ons 🎄",
+        supportingText: "Vier de feestdagen bij ons",
         btnTxt: "Bekijk ons kerstmenu",
         linkTo: "menu/christmas"
     });
