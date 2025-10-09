5cc159857e4a67869bf20836d6a3623a7373b0c3
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-10-09 14:57:33 +0200

    Finished the giftcard page and styled it

--- src/pages/Dinerbon.jsx
+++ src/pages/Dinerbon.jsx
@@ -1,10 +1,24 @@
 import "../styles/Dinerbon.css";
+import dinerbon from "../assets/imageGridImages/dinerbon.jpg";
 
 export default function Dinerbon() {
     return (
         <main>
             <div className="container">
-                <h1 className="font-size-32">Dinerbon</h1>
+                <h1 className="font-size-32 dinerbon-title">Dinerbon</h1>
+                <div className="dinerbon-container">
+                    <div>
+                        <h3>Op zoek naar het perfecte geschenk?</h3>
+                        <p>
+                            Verras familie, vrienden of collega's met een dinerbon voor een heerlijk avondje uit bij ons restaurant.
+                            Onze dinerbon is verkrijgbaar voor elk gewenst bedrag en biedt de ontvanger een culinaire beleving om nooit te vergeten.
+                        </p>
+                    </div>
+                    <div>
+                        <img src={dinerbon} alt="Onze prachtige dinerbon voor een vaas met bloemen, naast een fles wijn." />
+                        <p>* Voorbeeld van een dinerbon.</p>
+                    </div>
+                </div>
             </div>
         </main>
     );
