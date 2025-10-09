8ee9480d1479b466c561b9b865a89c96d910f078
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-10-09 15:13:19 +0200

    Replaced the dummy text

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -5,8 +5,8 @@ export default function Jobs() {
         <main>
             <div className="container">
                 <h1 className="font-size-32">Vacatures</h1>
-                <p>Wij zijn altijd op zoek naar extra medewerkers. Gelieve contact met ons opnemen via telefoon of email. Ook mag u eens gezellig langskomen voor een kopje koffie.</p>
-                <p>Als u via de mail contact opneemt, stuur dan een CV mee.</p>
+                <p>We zijn regelmatig op zoek naar nieuwe collega's die ons team willen versterken. Bel of mail ons gerust, of loop even binnen voor een kop koffie om kennis te maken.</p>
+                <p>Stuur bij een sollicitatie per e-mail ook uw cv mee.</p>
                 <ContactDetails />
             </div>
         </main>
