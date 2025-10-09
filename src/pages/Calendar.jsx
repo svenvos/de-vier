f51641ddf3540dcf94c7f707706702ec172c7509
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-10-09 16:21:03 +0200

    Added a file for all events, and made sure to use it on the calendar page

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,10 +1,29 @@
 import "../styles/Calendar.css";
+import { events } from "../utils/events";
 
 export default function Calendar() {
+    const eventElements = events.map(event => {
+        return (
+            <div key={event.id} className="event">
+                <h2>{event.title}</h2>
+                <p className="event-description">{event.description}</p>
+                <div className="event-details"> 
+                    <h3>Datum</h3>
+                    <p>{event.date}</p>
+                    <h3>Tijd</h3>
+                    <p>{event.time}</p>
+                </div>
+            </div>
+        );
+    });
+    
     return (
         <main>
             <div className="container">
                 <h1 className="font-size-32">Evenementen agenda</h1>
+                <div className="events-container">
+                    {eventElements}
+                </div>
             </div>
         </main>
     );
---
+++ src/utils/events.js
@@ -0,0 +1,25 @@
+import { nanoid } from "nanoid";
+
+export const events = [
+    {
+        id: nanoid(),
+        title: "Bandjesavond",
+        date: "25 oktober 2025",
+        description: "Geniet van een avond livemuziek van No Brakes",
+        time: "22:00 - 02:00"
+    },
+    {
+        id: nanoid(),
+        title: "Tiger Club",
+        date: "9 november 2025",
+        description: "Geniet van een feestelijke avond met Clemens en Hans",
+        time: "16:00"
+    },
+    {
+        id: nanoid(),
+        title: "Jazz avond",
+        date: "16 november 2025",
+        description: "Geniet van een avond jazzmuziek van de band The Jazz Cats",
+        time: "20:00 - 23:00"
+    }
+];
