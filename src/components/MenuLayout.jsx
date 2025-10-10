2adfb762e1cd2f6cd20b3c03807a90b694829f13
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-10-10 16:28:21 +0200

    Had to make a little change to the menu page, because we don't want to show the reservation button underneath the christmas menu

--- src/components/MenuLayout.jsx
+++ src/components/MenuLayout.jsx
@@ -1,15 +1,26 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import MenuNavbar from "./Menu/MenuNavbar.jsx";
 import "../styles/Menu.css";
 import { openWidget } from "../utils/widgetUtils.js";
+import ContactDetails from "./ContactDetails.jsx";
 
 export default function MenuLayout() {
+    const location = useLocation();
+    const isChristmasMenu = location.pathname === "/menu/christmas";
+
     return (
         <main>
             <div className="container">
                 <MenuNavbar />
                 <Outlet />
-                <button onClick={openWidget} className="btn sec-btn">Reserveer uw tafel</button>
+                {!isChristmasMenu ? (
+                    <button onClick={openWidget} className="btn sec-btn">Reserveer uw tafel</button>
+                ) : (
+                    <>
+                        <p>Reserveren voor kerst alleen mogelijk via telefoon of email.</p>
+                        <ContactDetails />
+                    </>
+                )}
             </div>
         </main>
     );
